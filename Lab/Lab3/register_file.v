b1f8c7ad5c676ab5bd3c11e57753e5b4ad7ffdc7
Griffin Tschurwald <griffin.tschurwald@gmail.com>
2015-11-14 11:46:38 -0500

    adding gitignore and some code for register

--- Lab/Lab3/register_file.v
+++ Lab/Lab3/register_file.v
@@ -1,8 +1,16 @@
 //register file for the single cycle cpu
 module register
+#(
+    parameter width = 32
+)
 (
-input WrEn
-input[31:0] Dw
+input WrEn,
+input[width-1:0] Dw,
+input[width-1:0] Aw,
+input[width-1:0] Aa,
+input[width-1:0] Ab,
+output[width-1:0] Da,
+output[width-1:0] Db 
 );
 
 endmodule
