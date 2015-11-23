ed281cc0ed03639860dad905f14ab302f64c750c
BrennaManning <brenna.manning@students.olin.edu>
2015-11-23 14:49:03 -0500

    corrected addi opcode

--- Lab/Lab3/instructiondecoder.v
+++ Lab/Lab3/instructiondecoder.v
@@ -194,9 +194,9 @@ module instructiondecoder(
 
 
  	  	//ADDI
- 	if (instruction [31:26] == 6'b100000)begin
+ 	if (instruction [31:26] == 6'b001000)begin
 		jal = 1'b0;
- 		regdst = 1'b0; //R-Type
+ 		regdst = 1'b0; //
  		branch = 1'b0;
  		jump = 1'b0;
  		jr = 1'b0;
